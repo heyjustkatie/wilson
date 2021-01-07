cb35f3e5c4743cf789bc0f2ca17ca8f1120c1cb3
Katie Santo <heyjustkatie@icloud.com>
2021-01-07 11:42:19 -0500

    updated game search

--- script.js
+++ script.js
@@ -124,67 +124,26 @@ const drinkingGame = document.getElementById('drinkingGame');
 const gameTypesArr = ["Card Game", "Board Game", "Skill Game", "Leisure", "Drinking Game"];
 var searchGameTypes = [];
 var gameTypeTrue = [];
-
 // gameTypesArr Index = 0 Card Game, 1 Board Game, 2 Skill Game, 3 Leisure, 4 Drinking Game
-
-// const gameTypesArr = [
-//   {
-//     id: cardGame,
-//     name: "Card Game",
-//     filter: "empty"
-//   },
-//   {
-//     id: boardGame,
-//     name: "Board Game",
-//     filter: "empty"
-//   },
-//   {
-//     id: skillGame,
-//     name: "Skill Game",
-//     filter: "empty"
-//   },
-//   {
-//     id: leisure,
-//     name: "Leisure",
-//     filter: "empty"
-//   },
-//   {
-//     id: drinkingGame,
-//     name: "Drinking Game",
-//     filter: "empty"
-//   }
-// ];
-
-// const searchGameTypes = [];
-// const gameTypeTrue = [];
-// const returnValue = array.filter((value, index, array) => {...}, thisArg);
-
-// const expensiveItems = [];
-
-// for (let i = 0 ; i < items.length; i++) {
-//   const item = items[i];
-//   if (item.price > 199) {
-//     expensiveItems.push(item);
-//   } 
-// }
 
 searchBtn.addEventListener("click", searchGames);
 refreshSearchBtn.addEventListener("click", refreshSearch);
 
 function searchGames() {
+  resultsContainer.innerHTML = "";
   Papa.parse('https://docs.google.com/spreadsheets/d/1deyiHAzodAjSE7kh40f4uod2lcM3f4M8bPdSiQy27Qs/pub?output=csv', {
     download: true,
     header: true,
     complete: function (results) {
       var data = results.data
       console.log("Getting games to search now");
-      searchType();
+      // searchType();
       filterGames(data)
     }
   })
 };
 
-function searchType() {
+function filterGames(gamesToFilter) {
   var searchGameTypes = [];
   console.log("Confirming game type now");
   var checkboxes = document.getElementsByClassName('gameChoice');
@@ -197,24 +156,27 @@ function searchType() {
     searchGameTypes.push("Card Game", "Board Game", "Skill Game", "Leisure", "Drinking Game");
   };
   console.log("Looking for " + searchGameTypes);
-};
-
-function filterGames(gamesToFilter) {
+  console.log(searchGameTypes[0]);
+  console.log(searchGameTypes[1]);
+  console.log(searchGameTypes[2]);
+  console.log(searchGameTypes[3]);
+  console.log(searchGameTypes[4]);
   console.log("Filtering games to show now");
   var gameTypeTrue = [];
   for (let i = 0; gamesToFilter.length > i; i++) {
-    if (gamesToFilter[i].Type == searchGameTypes[0])
+    console.log(gamesToFilter[i].Type);
+    if (gamesToFilter[i].Type === searchGameTypes[0])
       gameTypeTrue.push(gamesToFilter[i])
-    if (gamesToFilter[i].Type == searchGameTypes[1])
+    if (gamesToFilter[i].Type === searchGameTypes[1])
       gameTypeTrue.push(gamesToFilter[i])
-    if  (gamesToFilter[i].Type == searchGameTypes[2])
+    if  (gamesToFilter[i].Type === searchGameTypes[2])
       gameTypeTrue.push(gamesToFilter[i])
-    if  (gamesToFilter[i].Type == searchGameTypes[3])
+    if  (gamesToFilter[i].Type === searchGameTypes[3])
       gameTypeTrue.push(gamesToFilter[i])
-    if  (gamesToFilter[i].Type == searchGameTypes[4])
+    if  (gamesToFilter[i].Type === searchGameTypes[4])
       gameTypeTrue.push(gamesToFilter[i])
   };
-  searchResults(gamesToFilter);
+  searchResults(gameTypeTrue);
 };
 
 function searchResults(gameResults) {
@@ -228,240 +190,29 @@ function searchResults(gameResults) {
     var gameMinPlayers = gameResults[i].Min;
     var gameMaxPlayers = gameResults[i].Max;
     var gameNotes = gameResults[i].Notes;
-    if (gameMaxPlayers >= searchPlayerValue <= gameMaxPlayers) {
-      let gameEle = document.createElement('div');
-      let gameNameEle = document.createElement('h3');
-      let gameTypeEle = document.createElement('p');
-      let gamePlayersEle = document.createElement('p');
-      let gameNotesEle = document.createElement('p');
-      resultsContainer.appendChild(gameEle);
-      gameEle.setAttribute('class', 'gameItem');
-      gameEle.appendChild(gameNameEle);
-      gameEle.appendChild(gameTypeEle);
-      gameEle.appendChild(gamePlayersEle);
-      gameEle.appendChild(gameNotesEle);
-      gameNameEle.innerHTML = gameName;
-      gameTypeEle.innerHTML = gameType;
-      gamePlayersEle.innerHTML = gameMinPlayers + "-" + gameMaxPlayers + " players";
-      gameNotesEle.innerHTML = gameNotes;
+    if (searchPlayerValue >= gameMinPlayers) {
+      if (searchPlayerValue <= gameMaxPlayers) {
+        let gameEle = document.createElement('div');
+        let gameNameEle = document.createElement('h3');
+        let gameTypeEle = document.createElement('p');
+        let gamePlayersEle = document.createElement('p');
+        let gameNotesEle = document.createElement('p');
+        resultsContainer.appendChild(gameEle);
+        gameEle.setAttribute('class', 'gameItem');
+        gameEle.appendChild(gameNameEle);
+        gameEle.appendChild(gameTypeEle);
+        gameEle.appendChild(gamePlayersEle);
+        gameEle.appendChild(gameNotesEle);
+        gameNameEle.innerHTML = gameName;
+        gameTypeEle.innerHTML = gameType;
+        gamePlayersEle.innerHTML = gameMinPlayers + "-" + gameMaxPlayers + " players";
+        gameNotesEle.innerHTML = gameNotes;
+      }
+      else resultsContainer.innerHTML = "Sorry, no matching games found. Please try searching again!";
     }
-    else resultsContainer.innerHTML = "Sorry, no matching games found. Please try searching again!";
   };
   console.log("Search complete");
 };
-
-
-// old?
-
-// function searchType() {
-//   console.log("Confirming game type now");
-//   // var searchGameTypes = [];
-//   var checkboxes = document.getElementsByClassName('gameChoice');
-//   for (var checkbox of checkboxes) {
-//     if (checkbox.checked) {
-//       searchGameTypes.push(checkbox.name);
-//     };
-//   };
-//   if (searchGameTypes.length === 0) {
-//     searchGameTypes.push(gameTypesArr);
-//   };
-//   console.log("Looking for " + searchGameTypes);
-// };
-
-
-// ORIGINAL SEARCH TYPE FUNCTION
-
-// function searchType() {
-//   console.log("Confirming game type now");
-//   var searchGameTypes = [];
-//   var checkboxes = document.getElementsByClassName('gameChoice');
-//   for (var checkbox of checkboxes) {
-//     if (checkbox.checked) {
-//       searchGameTypes.push(checkbox.name);
-//     };
-//   };
-//   if (searchGameTypes.length === 0) {
-//     searchGameTypes.push(gameTypesArr);
-//   };
-//   console.log("Looking for " + searchGameTypes);
-// };
-
-// ORIGINAL FILTER FUNCTION
-
-// function filterGames(searchedGames) {
-//   console.log("Filtering games to show now");
-//   var searchPlayerValue = document.getElementById('playerSlider').value;
-//   console.log(searchPlayerValue + " players");
-//   resultsContainer.innerHTML = "";
-//   for (let i = 0; i < searchedGames.length; i++) {
-//     let gameName = searchedGames[i].Name;
-//     let gameType = searchedGames[i].Type;
-//     let gameMinPlayers = searchedGames[i].Min;
-//     let gameMaxPlayers = searchedGames[i].Max;
-//     if (gameName.includes(searchGameTypes)) {
-//       if (searchPlayerValue >= gameMinPlayers && searchPlayerValue <= gameMaxPlayers) {
-//         let gameEle = document.createElement('div');
-//         let gameNameEle = document.createElement('h3');
-//         let gameTypeEle = document.createElement('p');
-//         let gamePlayersEle = document.createElement('p');
-//         resultsContainer.appendChild(gameEle);
-//         gameEle.setAttribute('class', 'gameItem');
-//         gameEle.appendChild(gameNameEle);
-//         gameEle.appendChild(gameTypeEle);
-//         gameEle.appendChild(gamePlayersEle);
-//         gameNameEle.innerHTML = gameName;
-//         gameTypeEle.innerHTML = gameType;
-//         gamePlayersEle.innerHTML = gameMinPlayers + "-" + gameMaxPlayers + " players";
-//       };
-//     } else resultsContainer.innerHTML = "Sorry, no matching games found. Please try searching again!";
-//   };
-//   console.log("Search complete");
-// };
-
-// NEW FILTER FUNCTION - updated if order
-
-// function filterGames(searchedGames) {
-//   console.log("Filtering games to show now");
-//   // searchType(gameTypesArr);
-//   var searchPlayerValue = document.getElementById('playerSlider').value;
-//   console.log(searchPlayerValue + " players");
-//   resultsContainer.innerHTML = "";
-//   for (let i = 0; i < searchedGames.length; i++) {
-//     let gameName = searchedGames[i].Name;
-//     let gameType = searchedGames[i].Type;
-//     let gameMinPlayers = searchedGames[i].Min;
-//     let gameMaxPlayers = searchedGames[i].Max;
-//     if (gameName.includes(searchGameTypes)) {
-//       if (searchPlayerValue >= gameMinPlayers) {
-//         if (searchPlayerValue <= gameMaxPlayers) {
-//           let gameEle = document.createElement('div');
-//           let gameNameEle = document.createElement('h3');
-//           let gameTypeEle = document.createElement('p');
-//           let gamePlayersEle = document.createElement('p');
-//           resultsContainer.appendChild(gameEle);
-//           gameEle.setAttribute('class', 'gameItem');
-//           gameEle.appendChild(gameNameEle);
-//           gameEle.appendChild(gameTypeEle);
-//           gameEle.appendChild(gamePlayersEle);
-//           gameNameEle.innerHTML = gameName;
-//           gameTypeEle.innerHTML = gameType;
-//           gamePlayersEle.innerHTML = gameMinPlayers + "-" + gameMaxPlayers + " players";
-//         };
-//       }; 
-//     } else resultsContainer.innerHTML = "Sorry, no matching games found. Please try searching again!";
-//   };
-//   console.log("Search complete");
-// };
-
-
-// NEW FILTER FUNCTION UPDATING INCLUDES TO LOOP
-
-// var gameName = "";
-// var gameType = "";
-// var gameMinPlayers = "";
-// var gameMaxPlayers = "";
-
-
-// function filterGames(searchedGames) {
-//   console.log("Filtering games to show now");
-//   var searchPlayerValue = document.getElementById('playerSlider').value;
-//   console.log(searchPlayerValue);
-//   resultsContainer.innerHTML = "";
-//   for (let i = 0; i < searchedGames.length; i++) {
-//     var gameName = searchedGames[i].Name;
-//     var gameType = searchedGames[i].Type;
-//     var gameMinPlayers = searchedGames[i].Min;
-//     var gameMaxPlayers = searchedGames[i].Max;
-//     var gameNotes = searchedGames[i].Notes;
-//     function returnTrue() {
-//       const gameTypeTrue = [];
-//       for (let x = 0; x < searchGameTypes.length; x++)
-//       const gameTypeTrue = searchedGames
-//       .filter(searchedGames => searchedGames.Type == searchGameTypes[x])
-    
-//     };
-//     if (returnTrue()) {
-//       if (searchPlayerValue >= gameMinPlayers) {
-//         if (searchPlayerValue <= gameMaxPlayers) {
-//           let gameEle = document.createElement('div');
-//           let gameNameEle = document.createElement('h3');
-//           let gameTypeEle = document.createElement('p');
-//           let gamePlayersEle = document.createElement('p');
-//           let gameNotesEle = document.createElement('p');
-//           resultsContainer.appendChild(gameEle);
-//           gameEle.setAttribute('class', 'gameItem');
-//           gameEle.appendChild(gameNameEle);
-//           gameEle.appendChild(gameTypeEle);
-//           gameEle.appendChild(gamePlayersEle);
-//           gameEle.appendChild(gameNotesEle);
-//           gameNameEle.innerHTML = gameName;
-//           gameTypeEle.innerHTML = gameType;
-//           gamePlayersEle.innerHTML = gameMinPlayers + "-" + gameMaxPlayers + " players";
-//           gameNotesEle.innerHTML = gameNotes;          
-//         }
-//       }
-//     }
-//     else resultsContainer.innerHTML = "Sorry, no matching games found. Please try searching again!";
-//   };
-//   console.log("Search complete");
-// };
-
-// const returnValue = array.filter((value, index, array) => {...}, thisArg);
-
-// const expensiveItems = [];
-
-// for (let i = 0 ; i < items.length; i++) {
-//   const item = items[i];
-//   if (item.price > 199) {
-//     expensiveItems.push(item);
-//   } 
-// }
-
-
-// function include(arr, obj) {
-//   for (var i = 0; i < arr.length; i++) {
-//     if (arr[i] == obj) return true;
-//   }
-// }
-
-// function filterGames(searchedGames) {
-//   console.log("Filtering games to show now");
-//   var searchPlayerValue = document.getElementById('playerSlider').value;
-//   resultsContainer.innerHTML = "";
-//   for (let i = 0; i < searchedGames.length; i++) {
-//     var gameName = searchedGames[i].Name;
-//     var gameType = searchedGames[i].Type;
-//     var gameMinPlayers = searchedGames[i].Min;
-//     var gameMaxPlayers = searchedGames[i].Max;
-//     for (let x = 0; x < searchGameTypes.length; x++) {
-//       if (gameType.includes(searchGameTypes[i]) == 'true') {
-//         gameTypeTrue.push(gameName);
-//         console.log(gameName);
-//         console.log(searchPlayerValue);
-//         console.log(gameTypeTrue);
-//       }
-//     };
-//     console.log(gameTypeTrue);
-//     for (let y = 0; y < gameTypeTrue.length; y++) {
-//       if (searchPlayerValue >= gameMinPlayers && searchPlayerValue <= gameMaxPlayers) {
-//         let gameEle = document.createElement('div');
-//         let gameNameEle = document.createElement('h3');
-//         let gameTypeEle = document.createElement('p');
-//         let gamePlayersEle = document.createElement('p');
-//         resultsContainer.appendChild(gameEle);
-//         gameEle.setAttribute('class', 'gameItem');
-//         gameEle.appendChild(gameNameEle);
-//         gameEle.appendChild(gameTypeEle);
-//         gameEle.appendChild(gamePlayersEle);
-//         gameNameEle.innerHTML = gameName;
-//         gameTypeEle.innerHTML = gameType;
-//         gamePlayersEle.innerHTML = gameMinPlayers + "-" + gameMaxPlayers + " players";
-//       }
-//       else resultsContainer.innerHTML = "Sorry, no matching games found. Please try searching again!";
-//     };
-//   };
-//   console.log("Search complete");
-// };
 
 function refreshSearch() {
   resultsContainer.innerHTML = "";
@@ -572,10 +323,8 @@ function showGames(gamesAll) {
 // MANAGE GAMES
 
 const sheetsBtn = document.getElementById('sheetsBtn');
-// const sheetsUrl = "https://docs.google.com/spreadsheets/d/1deyiHAzodAjSE7kh40f4uod2lcM3f4M8bPdSiQy27Qs/edit";
+const sheetsUrl = "https://docs.google.com/spreadsheets/d/1deyiHAzodAjSE7kh40f4uod2lcM3f4M8bPdSiQy27Qs/edit";
 
 function sheetsBtnFn() {
-  window.location.href='https://docs.google.com/spreadsheets/d/1deyiHAzodAjSE7kh40f4uod2lcM3f4M8bPdSiQy27Qs/edit';
+  window.open(sheetsUrl)
 };
-
-// sheetsBtn.addEventListener = ("click", sheetsBtnFn);
